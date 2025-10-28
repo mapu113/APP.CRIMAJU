2e55e6f85bfb5c483b4f1146fd76be5ba32225df
Hidalgo-17 <ihidalgog@iegabo.edu.co>
2025-10-28 15:26:39 -0500

    ams

--- js/chatbot.js
+++ js/chatbot.js
@@ -38,7 +38,6 @@
         const respuestasSaludo = [
             "¡Hola! 😊 Soy tu chatbot de app.Crimaju. ¿En qué te puedo ayudar?",
             "¡Buenos días! 🌟 Puedo responder sobre **contacto**, **testimonios**, **servicios** y **dudas generales**.",
-            "¡Hola! 👋 ¿Qué te gustaría aprender sobre app.Crimaju hoy?"
         ];
 
         const respuestasDespedida = [
