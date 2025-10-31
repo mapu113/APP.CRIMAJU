36bc4c426c095475d6017e69a527fef9da2ae490
Hidalgo-17 <ihidalgog@iegabo.edu.co>
2025-10-30 19:11:04 -0500

    jlkakjs

--- js/script.js
+++ js/script.js
@@ -1,64 +1,45 @@
-$(document).ready(function() {
-
-$('.owl-carousel').owlCarousel({
-mouseDrag:false,
-loop:true,
-margin:2,
-nav:true,
-responsive:{
-0:{
-items:1
-},
-600:{
-items:1
-},
-1000:{
-items:3
-}
-}
+/**
+  Swiper:
+  https://swiperjs.com/
+**/
+const swiper = new Swiper(".swiperCarousel", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 10,
+  keyboard: {
+    enabled: true
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
 });
 
-$('.owl-prev').click(function() {
-$active = $('.owl-item .item.show');
-$('.owl-item .item.show').removeClass('show');
-$('.owl-item .item').removeClass('next');
-$('.owl-item .item').removeClass('prev');
-$active.addClass('next');
-if($active.is('.first')) {
-$('.owl-item .last').addClass('show');
-$('.first').addClass('next');
-$('.owl-item .last').parent().prev().children('.item').addClass('prev');
+const slides = document.getElementsByClassName("swiper-slide");
+for (const slide of slides) {
+  slide.addEventListener("click", () => {
+    const { className } = slide;
+    if (className.includes("swiper-slide-next")) {
+      swiper.slideNext();
+    } else if (className.includes("swiper-slide-prev")) {
+      swiper.slidePrev();
+    }
+  });
 }
-else {
-$active.parent().prev().children('.item').addClass('show');
-if($active.parent().prev().children('.item').is('.first')) {
-$('.owl-item .last').addClass('prev');
-}
-else {
-$('.owl-item .show').parent().prev().children('.item').addClass('prev');
-}
-}
-});
 
-$('.owl-next').click(function() {
-$active = $('.owl-item .item.show');
-$('.owl-item .item.show').removeClass('show');
-$('.owl-item .item').removeClass('next');
-$('.owl-item .item').removeClass('prev');
-$active.addClass('prev');
-if($active.is('.last')) {
-$('.owl-item .first').addClass('show');
-$('.owl-item .first').parent().next().children('.item').addClass('prev');
+function resizeTextToFit() {
+  const quoteEls = document.getElementsByClassName("quote");
+  for (const el of quoteEls) {
+    el.style.width = el.offsetWidth;
+    el.style.height = el.offsetHeight;
+  }
+  textFit(quoteEls, { maxFontSize: 14 });
 }
-else {
-$active.parent().next().children('.item').addClass('show');
-if($active.parent().next().children('.item').is('.last')) {
-$('.owl-item .first').addClass('next');
-}
-else {
-$('.owl-item .show').parent().next().children('.item').addClass('next');
-}
-}
-});
-
+resizeTextToFit();
+addEventListener("resize", (event) => {
+  resizeTextToFit();
 });
