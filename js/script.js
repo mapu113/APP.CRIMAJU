fd59ea3b526584aaffea79838ce386862d00bf68
Hidalgo-17 <ihidalgog@iegabo.edu.co>
2025-10-30 19:30:20 -0500

    jakjs

--- js/script.js
+++ js/script.js
@@ -1,45 +1,25 @@
-/**
-  Swiper:
-  https://swiperjs.com/
-**/
-const swiper = new Swiper(".swiperCarousel", {
-  slidesPerView: 3,
-  centeredSlides: true,
-  spaceBetween: 10,
-  keyboard: {
-    enabled: true
-  },
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination"
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-});
-
-const slides = document.getElementsByClassName("swiper-slide");
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    const { className } = slide;
-    if (className.includes("swiper-slide-next")) {
-      swiper.slideNext();
-    } else if (className.includes("swiper-slide-prev")) {
-      swiper.slidePrev();
-    }
-  });
-}
-
-function resizeTextToFit() {
-  const quoteEls = document.getElementsByClassName("quote");
-  for (const el of quoteEls) {
-    el.style.width = el.offsetWidth;
-    el.style.height = el.offsetHeight;
-  }
-  textFit(quoteEls, { maxFontSize: 14 });
-}
-resizeTextToFit();
-addEventListener("resize", (event) => {
-  resizeTextToFit();
-});
+jQuery(document).ready(function($) {
+        		"use strict";
+        		//  TESTIMONIALS CAROUSEL HOOK
+		        $('#customers-testimonials').owlCarousel({
+		            loop: true,
+		            center: true,
+		            items: 3,
+		            margin: 0,
+		            autoplay: true,
+		            dots:true,
+		            autoplayTimeout: 8500,
+		            smartSpeed: 450,
+		            responsive: {
+		              0: {
+		                items: 1
+		              },
+		              768: {
+		                items: 2
+		              },
+		              1170: {
+		                items: 3
+		              }
+		            }
+		        });
+        	});
